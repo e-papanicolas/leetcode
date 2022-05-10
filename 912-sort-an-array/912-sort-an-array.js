d29d4e9c974a485afb3e3a6dfb3ac5bbeee804e5
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-05-10 11:30:02 -0400

    Time: 6062 ms (12.22%), Space: 51.2 MB (98.83%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    
+  for(let i = 0; i < nums.length; i++) {
+      let min = i;
+      for(let j = i + 1; j < nums.length; j++) {
+          if(nums[j] < nums[min]) {
+              min = j;
+          }
+      }
+      let temp = nums[i];
+      nums[i] = nums[min];
+      nums[min] = temp;
+  }
+    return nums;
+};
