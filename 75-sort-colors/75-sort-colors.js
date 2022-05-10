20b0d22c852ef9a55e0590644f3cebf9eaf71cf0
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-05-10 10:54:04 -0400

    Time: 118 ms (7.81%), Space: 42.9 MB (7.77%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -3,12 +3,16 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = 0; j < nums.length; j++) {
+    let noSwaps;
+    for(let i = nums.length; i > 0; i--) {
+        noSwaps = true;
+        for(let j = 0; j < i - 1; j++) {
             if(nums[j] > nums[j + 1]) {
-                [nums[j], nums[j + 1]] = [nums[j+1], nums[j]]
+                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+                noSwaps = false;
             }
         }
+        if(noSwaps) break;
     }
     return nums;
 };
