d4984195319520fec03d6c650f601dbe685a051e
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-07-20 14:28:04 -0400

    Time: 94 ms (41.64%), Space: 42.2 MB (42.62%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if(nums[mid] === target) return mid;
+        
+        if(nums[start] <= nums[mid]) {
+             if(nums[mid] >= target && nums[start] <= target) {
+                 end = mid - 1;
+             } else {
+                 start = mid + 1;
+             }
+        } else {
+            if(nums[mid] <= target && nums[end] >= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
