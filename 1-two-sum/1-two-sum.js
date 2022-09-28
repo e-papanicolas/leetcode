6448a51971d38825471c83156c1d0f78785d1fae
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-09-28 10:10:16 -0400

    Update 1-two-sum.js

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,18 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let map = {};
     
-    let indexes = [];
-   
     for(let i = 0; i < nums.length; i++) {
-      
-        for (let j = i + 1; j < nums.length; j++) {
-            
-            if (nums[i] + nums[j] === target) {
-                indexes.push(i);
-                indexes.push(j);
-            }  
-        } 
+        let diff = target - nums[i];
+        
+        if(map[diff] !== undefined) {
+            return [i, map[diff]]
+        } else {
+            map[nums[i]] = i;
+        }
     }
-    return indexes;
 };
