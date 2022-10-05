cf840898e8dbbc996f3467b5152e6c8c1b371fd1
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-10-05 13:30:37 -0400

    Time: 135 ms (15.49%), Space: 44.2 MB (53.30%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(head === null) return null;
+    
+    let current = head;
+    
+    while(current && current.next) {
+       if(current.val === current.next.val) {
+           current.next = current.next.next;
+       } else {
+           current = current.next;
+       }
+    }
+    return head;
+};
