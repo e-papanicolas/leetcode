58382b6094734bc9ce45a5ac335756b795239dfc
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-10-12 16:38:30 -0400

    Time: 129 ms (17.87%), Space: 44.6 MB (11.05%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,22 +11,27 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let dummy = new ListNode();
-    let tail = dummy;
+    let temp = new ListNode();
+    let newHead = temp;
     
     while(list1 !== null && list2 !== null) {
+        
         if(list1.val < list2.val) {
-            tail.next = list1;
+           newHead.next = list1;
             list1 = list1.next;
         } else {
-            tail.next = list2;
+            newHead.next = list2;
             list2 = list2.next;
         }
-        tail = tail.next;
+        newHead = newHead.next;
     }
     
-    if(list1 !== null) tail.next = list1;
-    if(list2 !== null) tail.next = list2;
+    if(list1 !== null) {
+        newHead.next = list1;
+    }
+    if(list2 !== null) {
+        newHead.next = list2;
+    }
     
-    return dummy.next;
+    return temp.next;
 };
